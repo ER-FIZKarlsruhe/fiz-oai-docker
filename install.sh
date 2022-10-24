651c5d395f3c71622f29bc64b02ee516047a9a16
sth <stefan.hofmann@fiz-karlsruhe.de>
2022-10-24 14:04:03 +0200

    Set owner

--- install.sh
+++ install.sh
@@ -7,30 +7,29 @@ rm -rf ${INSTALL_DIR}
 ###############################################################################
 
 mkdir -p ${INSTALL_DIR}/configs/cassandra/
-chown -R 999:999 ${INSTALL_DIR}/configs/cassandra/
-
 mkdir -p ${INSTALL_DIR}/data/cassandra/
-chown -R 999:999 ${INSTALL_DIR}/data/cassandra/
 
 cp ./configs/cassandra.yaml ${INSTALL_DIR}/configs/cassandra/
 cp ./configs/cassandra-env.sh ${INSTALL_DIR}/configs/cassandra/
 cp ./configs/init-fizoai-database.sh ${INSTALL_DIR}/configs/cassandra/
 
+chown -R 999:999 ${INSTALL_DIR}/data/
+chown -R 999:999 ${INSTALL_DIR}/configs/
 
 ###############################################################################
 # Init Elasticsearch. The container runs under the user_id 1000
 ###############################################################################
 
 mkdir -p ${INSTALL_DIR}/configs/elasticsearch_oai/
-chown -R 1000:1000 ${INSTALL_DIR}/configs/elasticsearch_oai/
-
-mkdir -p ${INSTALL_DIR}/data/elasticsearch_oai/
-chown -R 1000:1000 ${INSTALL_DIR}/data/elasticsearch_oai/
-
+mkdir -p ${INSTALL_DIR}/data/elasticsearch_oai/es-data/
+mkdir -p ${INSTALL_DIR}/data/elasticsearch_escidocng/backup/
 mkdir -p ${INSTALL_DIR}/logs/elasticsearch_oai/
-chown -R 1000:1000 ${INSTALL_DIR}/logs/elasticsearch_oai/
 
 cp ./configs/oai-elasticsearch.yml ${INSTALL_DIR}/configs/elasticsearch_oai/
+
+chown -R 1000:1000 ${INSTALL_DIR}/data/elasticsearch_oai/
+chown -R 1000:1000 ${INSTALL_DIR}/configs/elasticsearch_oai/
+chown -R 1000:1000 ${INSTALL_DIR}/logs/elasticsearch_oai/
 
 ###############################################################################
 # Init oai_backend. The container runs under the user_id 1000
