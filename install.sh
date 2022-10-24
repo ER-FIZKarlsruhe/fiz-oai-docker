e6f40cd8296a6b718a3f47210e70b30b99c505cb
sth <stefan.hofmann@fiz-karlsruhe.de>
2022-10-24 12:14:42 +0200

    Add properties for oai-provider and oai-backend

--- install.sh
+++ install.sh
@@ -8,10 +8,20 @@ mkdir -p ${INSTALL_DIR}/data/cassandra/
 mkdir -p ${INSTALL_DIR}/configs/elasticsearch_oai/
 mkdir -p ${INSTALL_DIR}/data/elasticsearch_oai/
 
+mkdir -p ${INSTALL_DIR}/configs/oai_backend/
+mkdir -p ${INSTALL_DIR}/data/oai_backend/
+
+mkdir -p ${INSTALL_DIR}/configs/oai_provider/
+mkdir -p ${INSTALL_DIR}/data/oai_provider/
+
 cp ./configs/cassandra.yaml ${INSTALL_DIR}/configs/cassandra/
 cp ./configs/cassandra-env.sh ${INSTALL_DIR}/configs/cassandra/
 
 cp ./configs/oai-elasticsearch.yml ${INSTALL_DIR}/configs/elasticsearch_oai/
+
+cp ./configs/fiz-oai-backend.properties ${INSTALL_DIR}/configs/oai_backend/
+
+cp ./configs/oaicat.properties ${INSTALL_DIR}/configs/oai_provider/
 
 cp docker-compose.yml ${INSTALL_DIR}
 cp ./configs/.env ${INSTALL_DIR}
