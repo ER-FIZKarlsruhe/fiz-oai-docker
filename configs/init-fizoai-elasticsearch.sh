30a207668d549c71ff38b224dcb05a936159aa25
sth <stefan.hofmann@fiz-karlsruhe.de>
2022-10-25 12:14:08 +0200

    Add Tomcat parameters for allowing / paths

--- configs/init-fizoai-elasticsearch.sh
+++ configs/init-fizoai-elasticsearch.sh
@@ -1,8 +1,5 @@
-#############################################################
-# Init ElasticSearch
-#############################################################
-echo "Waiting for Elasticsearch...";
-sleep 40s;
+echo "Waiting for Elasticsearch fully initialized..."
+sleep 40
 
-curl -v -X PUT -H 'Content-Type: application/json' --data "@item_mapping_es_v7" -i 'http://elasticsearch-oai:9200/item'
+curl -v -X PUT -H 'Content-Type: application/json' --data "@item_mapping_es_v7" -i 'http://elasticsearch-oai:9200/items'
 
---
+++ examples/createFormats.sh
@@ -0,0 +1,2 @@
+curl -X POST -H 'Content-Type: application/json' -i 'http://eng-d-vm08.fiz-karlsruhe.de:8081/oai-backend/format' --data '{"metadataPrefix":"oai_dc","schemaLocation":"http://www.openarchives.org/OAI/2.0/oai_dc.xsd","schemaNamespace":"http://www.openarchives.org/OAI/2.0/oai_dc/","identifierXpath":"/identifier"}'
+
