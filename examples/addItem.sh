1f6afa548ad3e3307b8f5de5f53f06ffba7f94aa
sth <stefan.hofmann@fiz-karlsruhe.de>
2022-10-26 10:32:33 +0200

    changes

--- examples/addItem.sh
+++ examples/addItem.sh
@@ -3,4 +3,4 @@
 curl -X POST -H -i 'http://localhost:8081/oai-backend/item' -d content=@10.5072-38238.xml --data metadataJson='{"identifier":" 10.5072/38238","deleteFlag":false,"ingestFormat":"radar"}'
 
 
-curl -v -X POST -H 'Content-Type: multipart/form-data' -i 'http://localhost:8081/oai-backend/item'  -F item=@10.5072-38238-item.json -F content=@10.5072-38238.xml
+curl -v -X POST -H 'Content-Type: multipart/form-data' -i 'http://localhost:8081/oai-backend/item'  -F item=@./10.5072-38238-item.json -F content=@./10.5072-38238.xml
