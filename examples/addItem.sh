c313cc3e2aae5b5e423bd5fc8e9f086691315aee
sth <stefan.hofmann@fiz-karlsruhe.de>
2022-10-26 10:21:22 +0200

    Improve examples

--- examples/addItem.sh
+++ examples/addItem.sh
@@ -1,3 +1,6 @@
 #Create Item for 10.5072-38238.xml
 
-curl -X POST -H 'Content-Type: application/json' -i 'http://localhost:8081/oai-backend/item' -d @10.5072-38238.xml --data "{"identifier":" 10.5072/38238","deleteFlag":false,"ingestFormat":"radar"}"
+curl -X POST -H -i 'http://localhost:8081/oai-backend/item' -d content=@10.5072-38238.xml --data metadataJson='{"identifier":" 10.5072/38238","deleteFlag":false,"ingestFormat":"radar"}'
+
+
+curl -v -X POST -H 'Content-Type: multipart/form-data' -i 'http://localhost:8081/oai-backend/item'  -F item=@10.5072-38238-item.json -F content=@10.5072-38238.xml
