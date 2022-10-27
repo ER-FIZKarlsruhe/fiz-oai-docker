c1675c3a93d135267671bac31c36e72ea581846e
sth <stefan.hofmann@fiz-karlsruhe.de>
2022-10-27 12:22:37 +0200

    Bugfix

--- configs/init-fizoai-elasticsearch.sh
+++ configs/init-fizoai-elasticsearch.sh
@@ -1,5 +1,10 @@
 echo "Waiting for Elasticsearch fully initialized..."
-sleep 40
+status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost:9200/_cluster/health?wait_for_status=green&timeout=120s)
 
-curl -v -X PUT -H 'Content-Type: application/json' --data "@item_mapping_es_v7" -i 'http://elasticsearch-oai:9200/items'
+if [[ "$status_code" -ne 200 ]] ; then
+  echo "Site status changed to $status_code"
+else
+  curl -v -X PUT -H 'Content-Type: application/json' --data "@item_mapping_es_v7" -i 'http://elasticsearch-oai:9200/items'
+  exit 0
+fi
 
