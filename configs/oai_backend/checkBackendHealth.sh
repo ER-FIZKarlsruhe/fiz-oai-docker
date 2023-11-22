0a284365a537ff314855e359de4623bc9ccbe1f0
Michael Hoppe <michael.hoppe@fiz-karlsruhe.de>
2023-11-22 10:45:57 +0100

    OAI-123: Improve docker-compose of fiz-oai-docker

---
+++ configs/oai_backend/checkBackendHealth.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+URL="http://oai-backend:8080/oai-backend/info/version"
+
+response=$(curl -L --noproxy '*' --write-out %{http_code} --silent --output /dev/null $URL)
+if [[ "$response" -eq 200 ]]; then
+  echo "Backend Status is OK"
+  exit 0
+else
+  echo "Waiting for Backend Status OK"
+  exit 1
+fi
+
